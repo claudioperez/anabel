f9beef002bc8b8520f7a3e5dccac9bd08b05b8b2
claudioperezii <58619438+claudioperezii@users.noreply.github.com>
2020-01-20 12:40:08 -0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,35 @@
+"""
+Earthquake Matrix Analysis. (:mod:`ema`)
+=====================================================
+
+.. currentmodule:: ema
+
+
+"""
+
+
+# __all__ = ['a', 'b', 'c']
+__version__ = '0.0.2'
+__author__ = None
+
+
+import ema.analysis
+import ema.sections
+from ema.objects import *
+# from ema.matvecs import *
+from ema.matrices import *
+
+from ema.graphics import *
+from ema.solvers import *
+
+import ema.utilities
+# from ema.utilities.fedeas.syntax import *
+from ema.tests import *
+
+
+settings = {
+    "DATAFRAME_LATEX": True,
+
+}
+
+
