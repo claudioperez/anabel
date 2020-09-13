ae2f47ae6c71b3ab4a7d3a069e929b9d6d4ea02b
claudioperez <50180406+claudioperez@users.noreply.github.com>
2020-09-12 18:28:33 -0700

    Modify autodiff API

--- src/anabel/autodiff/jaxapi.py
+++ src/anabel/autodiff/jaxapi.py
@@ -1,13 +1,35 @@
+from typing import Callable, Union, Sequence
 import jax
 import jax.numpy as jnp
-from jax import jacfwd, jacrev, grad
+from jax import grad
 
 
+def jacfwd(fun: Callable, outnums: Union[int, Sequence[int]] = None,
+            argnums: Union[int,Sequence[int]]=0, *,
+            holomorphic: bool = False, squeeze=True, nf=None) -> Callable:
+    if outnums is None:
+        if nf is None:
+            jac = jax.jacfwd(fun, argnums, holomorphic)
+        else:
+            jac = jax.jacfwd(lambda *args,**kwds: fun(*args,**kwds)[:nf,:nf], argnums, holomorphic)
+    else:
+        jac = jax.jacfwd(lambda *args,**kwds: fun(*args,**kwds)[outnums],argnums,holomorphic)
+    if squeeze:
+        return lambda *args, **kwds: jnp.squeeze(jac(*args,**kwds))
+    else:
+        return jac
 
-
-def potential_grad(func):
-    pass
-
+def jacrev(fun: Callable, outnums: Union[int, Sequence[int]] = None,
+            argnums: Union[int,Sequence[int]]=0, *,
+            holomorphic: bool = False, squeeze=True) -> Callable:
+    if outnums is None:
+        jac = jax.jacrev(fun, argnums, holomorphic)
+    else:
+        jac = jax.jacrev(lambda *args,**kwds: fun(*args,**kwds)[outnums],argnums,holomorphic)
+    if squeeze:
+        return lambda *args, **kwds: jnp.squeeze(jac(*args,**kwds))
+    else:
+        return jac
 
 def stiffness_matrix(f, nf, mode='fwd',jit=False):
     F = lambda x, **params: f(x,**params)
