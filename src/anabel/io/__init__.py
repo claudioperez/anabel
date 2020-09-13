0c837a623c42a0eddd434ecc9a629ad63e2232c5
claudioperez <50180406+claudioperez@users.noreply.github.com>
2020-09-12 18:27:48 -0700

    Switched from ruamel.yaml to PyYaml

--- src/anabel/io/__init__.py
+++ src/anabel/io/__init__.py
@@ -5,26 +5,29 @@ from anabel import COMPILER
 compiler = COMPILER
 
 
-try:
-    from ruamel.yaml import YAML
-except ImportError:
-    yaml = None
-else: 
-    yaml = YAML(typ='safe')
-
+# try:
+#     from ruamel.yaml import YAML
+# except ImportError:
+#     yaml = None
+# else: 
+#     yaml = YAML()
+#     yaml.indent(mapping=2, sequence=4, offset=2)
+import yaml
 
 def _yaml_load(filepath):
-    with open(filepath) as f: model_dict = yaml.load(f)
+    #with open(filepath) as f: model_dict = yaml.load(f) # ruamel.yaml
+    with open(filepath) as f: model_dict = yaml.load(f,Loader=yaml.FullLoader) # PyYaml
     return model_dict
 
 def load(filepath):
     """Load from a serialized data file"""
     _, file_ext = os.path.splitext(filepath)
     
-    if file_ext in ['.yml', '.yaml']: 
+    if file_ext in ['.yml', '.yaml']:
         return _yaml_load(filepath)
 
-def dump(filepath): pass
+def dump_yaml(doc, fstream):
+    yaml.dump(doc, fstream)
 
 def compile(func,inputs):
     if compiler is not None: 
