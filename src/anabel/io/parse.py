edaf2af62704609d48ff0cced91e39c2ed85bc27
claudioperez <50180406+claudioperez@users.noreply.github.com>
2020-11-03 11:47:25 -0800

    cmp - Develop io module.

---
+++ src/anabel/io/parse.py
@@ -0,0 +1,15 @@
+import yaml
+
+
+def parseYamlFile(FileName, gc=None, lc=None,**kwds):
+    with open(FileName,'r') as f: file_str = f.read()
+
+    compiled_str = compile('fr"""'+file_str+'"""', '<file_str>', 'eval')
+
+    formated_str = eval(compiled_str, {**gc, **kwds}, lc)
+
+    return yaml.load(formated_str, Loader=yaml.Loader)
+
+if __name__=='__main__':
+
+    print(parseYamlFile('test.yaml', gc={"L":20}, E=29e3))
